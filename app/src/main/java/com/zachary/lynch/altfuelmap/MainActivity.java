fb73b48782c70b7ad5c409837e1c398919da5bce
Unknown <Zachary.Lynch1337@gmail.com>
2018-04-04 10:25:02 -0800

    Added a method to check if network is available

--- app/src/main/java/com/zachary/lynch/altfuelmap/MainActivity.java
+++ app/src/main/java/com/zachary/lynch/altfuelmap/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zachary.lynch.altfuelmap;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,16 +9,33 @@ import android.os.Bundle;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // hid api key from GitHub
         ApiKey apiKey = new ApiKey();
 
-        // build the url
+        // TODO: 1. build the url
         String fuelUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=YOUR_KEY_HERE"
                 + apiKey.getAPI_KEY();
+        if (isNetworkAvailable()){
+            //TODO: 3. use OkHttp to get an Asynchronous get to get data from api
 
+        }
+
+    }
+    // TODO: 2. Use Connectivity manager to see if the network is available
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (info != null && info.isConnected()){
+            isAvailable = true;
+        }
+        return isAvailable;
     }
 }
