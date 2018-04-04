822f4189485d08190a6b7f490d2acbc0c6351944
Unknown <Zachary.Lynch1337@gmail.com>
2018-04-04 10:40:36 -0800

    Added a new class to get and set all the data from the JSON data

---
+++ app/src/main/java/com/zachary/lynch/altfuelmap/StationData.java
@@ -0,0 +1,123 @@
+package com.zachary.lynch.altfuelmap;
+
+
+
+public class StationData {
+    private String mFuel_type_code;
+    private String mStation_Phone;
+    private String mStation_Name;
+    private String mLatitude;
+    private String mLongitude;
+    private String mCity;
+    private String mState;
+    private String mStreet_address;
+    private String mZip;
+    private String mCountry;
+    private String mHasE85;
+    private String mE85;
+    private String mElectirc;
+
+    public String getFuel_type_code() {
+        return mFuel_type_code;
+    }
+
+    public void setFuel_type_code(String fuel_type_code) {
+        mFuel_type_code = fuel_type_code;
+    }
+
+    public String getStation_Phone() {
+        return mStation_Phone;
+    }
+
+    public void setStation_Phone(String station_Phone) {
+        mStation_Phone = station_Phone;
+    }
+
+    public String getStation_Name() {
+        return mStation_Name;
+    }
+
+    public void setStation_Name(String station_Name) {
+        mStation_Name = station_Name;
+    }
+
+    public String getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(String latitude) {
+        mLatitude = latitude;
+    }
+
+    public String getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(String longitude) {
+        mLongitude = longitude;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public void setState(String state) {
+        mState = state;
+    }
+
+    public String getStreet_address() {
+        return mStreet_address;
+    }
+
+    public void setStreet_address(String street_address) {
+        mStreet_address = street_address;
+    }
+
+    public String getZip() {
+        return mZip;
+    }
+
+    public void setZip(String zip) {
+        mZip = zip;
+    }
+
+    public String getCountry() {
+        return mCountry;
+    }
+
+    public void setCountry(String country) {
+        mCountry = country;
+    }
+
+    public String getHasE85() {
+        return mHasE85;
+    }
+
+    public void setHasE85(String hasE85) {
+        mHasE85 = hasE85;
+    }
+
+    public String getE85() {
+        return mE85;
+    }
+
+    public void setE85(String e85) {
+        mE85 = e85;
+    }
+
+    public String getElectirc() {
+        return mElectirc;
+    }
+
+    public void setElectirc(String electirc) {
+        mElectirc = electirc;
+    }
+}
