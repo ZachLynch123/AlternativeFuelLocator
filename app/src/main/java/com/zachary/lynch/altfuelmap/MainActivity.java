0dfd219906d39f116e132c52ed8d98349d125ac8
Unknown <Zachary.Lynch1337@gmail.com>
2018-04-04 10:18:44 -0800

    Added permissions to manifest

--- app/src/main/java/com/zachary/lynch/altfuelmap/MainActivity.java
+++ app/src/main/java/com/zachary/lynch/altfuelmap/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zachary.lynch.altfuelmap;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ApiKey apiKey = new ApiKey();
+
+        // build the url
+        String fuelUrl = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=YOUR_KEY_HERE"
+                + apiKey.getAPI_KEY();
+
     }
 }
